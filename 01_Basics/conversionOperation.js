c53bd9c3782a99398cf3313dd3cec0c5c0136d60
Shashank <167966056+Coder-Shanky@users.noreply.github.com>
2025-07-25 18:32:20 +0000

    type of

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -1,3 +1,22 @@
-let score = 55
+let score = undefined
 
 console.log(typeof score);
+
+let valueInnumber = Number (score);
+
+console.log(typeof valueInnumber);
+
+console.log(valueInnumber);
+
+
+
+// "45" => 33
+// "45abc" => NaN
+// true => 1; false =>0
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log( typeof stringNumber);
