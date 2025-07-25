ce656d003a6c2df8987cddd783ba3430ac5d529c
Shashank <167966056+Coder-Shanky@users.noreply.github.com>
2025-07-25 18:51:47 +0000

    Start Operations

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -1,12 +1,12 @@
 let score = undefined
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInnumber = Number (score);
 
-console.log(typeof valueInnumber);
+// console.log(typeof valueInnumber);
 
-console.log(valueInnumber);
+// console.log(valueInnumber);
 
 
 
@@ -18,5 +18,18 @@ let someNumber = 33
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log( typeof stringNumber); 
+// console.log(stringNumber);
+// console.log( typeof stringNumber); 
+
+// *********************** Operations ***********************
+
+let str1 = "Hello"
+
+let str2 = "World"
+
+let str3 = str1 + str2 
+
+console.log(str3);
+
+
+
