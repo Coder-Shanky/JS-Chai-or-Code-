0a832911f423701d6ae98f93df067ad8b15d7bc5
Shashank <167966056+Coder-Shanky@users.noreply.github.com>
2025-07-28 18:58:17 +0000

    add expression

---
+++ 01_Basics/expression and operators.js
@@ -0,0 +1,15 @@
+// Practice Questions
+
+// let result = "hello" / 2 
+// // console.log(result);
+
+
+// let result2 = 0.1 + 0.2
+// // console.log(result2);
+
+// const score = 55 * "hello"
+// console.log(score);
+
+// console.log(typeof score);
+
+console.log("5" + 3);
